08c1343b1fde24a63f951995ea849e20f30d19b8
YusufJake <yusufjohan05@gmail.com>
2020-12-21 20:47:52 +0700

    Service Rest sudah siap

---
+++ ITI_UMY.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+
+namespace ServiceRest_20180140067_Yusuf_Johan_Kelana
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ITI_UMY
+    {
+//        [OperationContract]
+//        string GetData(int value);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Mahasiswa", ResponseFormat = WebMessageFormat.Json)] // untuk membuat slash, selalu relative
+        List<Mahasiswa> GetAllMahasiswa(); // Mendapatkan Kumpulan mahasiswa / seluruh data mahasiswa
+
+        /* Nama Method */
+        [OperationContract]
+        [WebGet(UriTemplate = "Mahasiswa/nim={nim}", ResponseFormat = WebMessageFormat.Json)] // untuk get
+        Mahasiswa GetMahasiswaByNIM(string nim); // Mengambil data berdasarkan nim
+
+        // void CreateMahasiswa(Mahasiswa mhs), tidak ada pengembalian / respond balik
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Mahasiswa", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)] // untuk membuat slash, selalu relative
+        string CreateMahasiswa(Mahasiswa mhs); // Mendapatkan Kumpulan mahasiswa / seluruh data mahasiswa
+
+        //        [OperationContract]
+        //        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceRest_20180140067_Yusuf_Johan_Kelana.ContractType".
+
+    [DataContract]
+    public class Mahasiswa
+    {
+        // Variabel lokal - Konvensi atau kesepakatan
+        private string _nama, _nim, _prodi, _angkatan;
+
+        // Mengirim data untuk mengurutkan
+        [DataMember(Order = 1)]
+        public string nim
+        {
+            get { return _nim; }
+            set { _nim = value; }
+        }
+
+        [DataMember(Order = 2)]
+        public string nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+
+        [DataMember(Order = 3)]
+        public string prodi
+        {
+            get { return _prodi; }
+            set { _prodi = value; }
+        }
+
+        [DataMember(Order = 4)]
+        public string angkatan
+        {
+            get { return _angkatan; }
+            set { _angkatan = value; }
+        }
+
+    }
+
+    //    [DataContract]
+    //    public class CompositeType
+    //    {
+    //        bool boolValue = true;
+    //        string stringValue = "Hello ";
+
+    //        [DataMember]
+    //        public bool BoolValue
+    //        {
+    //            get { return boolValue; }
+    //            set { boolValue = value; }
+    //        }
+
+    //        [DataMember]
+    //        public string StringValue
+    //        {
+    //            get { return stringValue; }
+    //            set { stringValue = value; }
+    //        }
+    //    }
+}
---
+++ TI_UMY.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.SqlClient;
+
+
+namespace ServiceRest_20180140067_Yusuf_Johan_Kelana
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class TI_UMY : ITI_UMY
+    {
+        public string CreateMahasiswa(Mahasiswa mhs)
+        {
+            string msg = "Gagal";
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-SU3D3TD;Initial Catalog=\"TI UMY\";User ID=sa;Password=goldensilk2020");
+            string query = string.Format("insert into dbo.Mahasiswa values ('{0}', '{1}', '{2}', '{3}')", mhs.nim, mhs.nama, mhs.prodi, mhs.angkatan);
+            // NIM = '{0}', nim
+            // string query = "insert into dbo.Mahasiswa values ('"+mhs.nim+"', '"+mhs.nama+"', '"+mhs.prodi+"', '"+mhs.angkatan+"')";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open(); // Membuka koneksi SQL Server
+                Console.WriteLine(query);
+                command.ExecuteNonQuery(); //mengeksekusi untuk memasukkan data
+                connection.Close();
+                msg = "Sukses";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+                msg = "GAGAL";
+            }
+
+            return msg;
+        }
+
+        public List<Mahasiswa> GetAllMahasiswa()
+        {
+            List<Mahasiswa> mahasiswas = new List<Mahasiswa>();
+
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-SU3D3TD;Initial Catalog=\"TI UMY\";User ID=sa;Password=goldensilk2020");
+            string query = "select NIM, Nama, Prodi, Angkatan from dbo.Mahasiswa";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Mahasiswa mahasiswa = new Mahasiswa();
+                    mahasiswa.nim = reader.GetString(0); // 0 itu array pertama dan diambil dari ITI_UMY
+                    mahasiswa.nama = reader.GetString(1);
+                    mahasiswa.prodi = reader.GetString(2);
+                    mahasiswa.angkatan = reader.GetString(3);
+
+                    mahasiswas.Add(mahasiswa);
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+
+            return mahasiswas; // Output
+        }
+
+//        public string GetData(int value)
+//        {
+//            return string.Format("You entered: {0}", value);
+//        }
+
+        public Mahasiswa GetMahasiswaByNIM(string nim)
+        {
+            Mahasiswa mahasiswa = new Mahasiswa();
+
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-SU3D3TD;Initial Catalog=\"TI UMY\";User ID=sa;Password=goldensilk2020");
+            string query = string.Format("select NIM, Nama, Prodi, Angkatan from dbo.Mahasiswa where NIM = '{0}'", nim);
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    mahasiswa.nim = reader.GetString(0); // 0 itu array pertama dan diambil dari ITI_UMY
+                    mahasiswa.nama = reader.GetString(1);
+                    mahasiswa.prodi = reader.GetString(2);
+                    mahasiswa.angkatan = reader.GetString(3);
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+
+            return mahasiswa; // Output
+        }
+
+        //        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //        {
+        //            if (composite == null)
+        //            {
+        //                throw new ArgumentNullException("composite");
+        //            }
+        //            if (composite.BoolValue)
+        //            {
+        //                composite.StringValue += "Suffix";
+        //            }
+        //            return composite;
+        //        }
+    }
+}
